1f8647b5d551834aa66e1f3a5b2090249d254c2d
Gustavo Neves <guneves@gmail.com>
2016-04-10 08:22:54 -0300

    fix indenting

--- app/code/community/Rayfox/Catalog/Model/Search/Advanced.php
+++ app/code/community/Rayfox/Catalog/Model/Search/Advanced.php
@@ -19,17 +19,17 @@ class Rayfox_Catalog_Model_Search_Advanced extends Mage_CatalogSearch_Model_Adva
 		if(!Mage::helper('rayfox_catalog')->isEnabledForSearchResults()){
 			return $this;
 		}
-        $websiteId = Mage::app()->getStore()->getWebsiteId();
+		$websiteId = Mage::app()->getStore()->getWebsiteId();
 		if (Mage::helper('catalog')->isModuleEnabled('Mage_CatalogInventory')) {
-            $collection->joinTable(
-                array('cisi' => 'cataloginventory/stock_status'),
-                'product_id=entity_id',
-                array('stock_status'),
-                 array('website_id'=> $websiteId),
-                'left'
-            );
-        }
-        $collection->getSelect()->order('stock_status desc');
+			$collection->joinTable(
+				array('cisi' => 'cataloginventory/stock_status'),
+				'product_id=entity_id',
+				array('stock_status'),
+				array('website_id'=> $websiteId),
+				'left'
+			);
+		}
+		$collection->getSelect()->order('stock_status desc');
 		return $this;
 	}
 }
